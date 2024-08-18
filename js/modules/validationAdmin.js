ef0aeb2b0864d60aa3f8ca57c7c7366ac6f6a3a3
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-08-18 18:05:13 -0300

    add: validacion del form del administrado para los input de numeros y la validacion del input de dimensiones de los libros

--- js/modules/validationAdmin.js
+++ js/modules/validationAdmin.js
@@ -11,7 +11,7 @@ const inputPrice = document.querySelector("#price");
 const inputStock = document.querySelector("#stock");
 const inputDescription = document.querySelector("#description");
 
-// fn ---> crea un div con un mensaje de error en el input
+// fn --> crea un div con un mensaje de error en el input
 const msjInvalidInput = (msj, childNode, createMsj = true) => {
   const parentNode = childNode.parentNode;
 
@@ -32,7 +32,7 @@ const msjInvalidInput = (msj, childNode, createMsj = true) => {
   }
 };
 
-// fn ---> valida la longitud del input enviado como parámetro y si es requerido o no
+// fn --> valida la longitud del input enviado como parámetro y si es requerido o no
 const validateLength = (
   nameInput = "input",
   input,
@@ -69,7 +69,7 @@ const validateLength = (
   return true;
 };
 
-// fn ---> valida si el input de la portada es una URL valida
+// fn --> valida si el input de la portada es una URL valida
 const validateUrl = (input) => {
   const regexUrl = /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|bmp|webp|svg))$/i;
   const msjUrlInvalid = `Por favor ingrese una URL de imagen valida`;
@@ -92,7 +92,7 @@ const validateUrl = (input) => {
   }
 };
 
-// fn ---> valida si el código ISBN es correcto
+// fn --> valida si el código ISBN es correcto
 const validateCodeIsbn = (input) => {
   const alphabetRegex = /[a-zA-Z]/.test(input.value);
   const symbolsRegex = /[^a-zA-Z0-9-]/.test(input.value);
@@ -111,16 +111,75 @@ const validateCodeIsbn = (input) => {
   return true;
 };
 
+// fn --> verifica si el dato del input son numero y no string
+const validateNumber = (input) => {
+  const inputValue = parseFloat(input.value);
+  const msjNumberInvalid = `El contenido tiene que ser numérico`;
+
+  if (Number.isNaN(inputValue)) {
+    msjInvalidInput(msjNumberInvalid, input);
+    input.className = "form-control";
+    input.classList.add("is-invalid");
+    input.focus();
+    return false;
+  }
+
+  msjInvalidInput("N/D", input, false);
+  input.className = "form-control";
+  return true;
+};
+
+// fn --> valida el formato del input de dimensiones de los libros
+const validateFormDimensions = (input) => {
+  const dimensionsRegex = /^\s*\d+,\d+\s*x\s*\d+,\d+\s*$/i;
+  const msjDimensionsInvalid = `El formato de las dimensiones es ( 15,5 x 22,5 )`;
+
+  if (dimensionsRegex.test(input.value)) {
+    msjInvalidInput("N/D", input, false);
+    input.className = "form-control";
+    return true;
+  } else {
+    msjInvalidInput(msjDimensionsInvalid, input);
+    input.className = "form-control";
+    input.classList.add("is-invalid");
+    input.focus();
+    return false;
+  }
+};
+
+// fn --> valida todos los input del formulario y devuelve un booleano dependiendo
 const validateFormAdmin = () => {
   const validateFromBookUrl =
     validateLength("link", inputFromBookUrl, 8, 100, false) &&
     validateUrl(inputFromBookUrl);
   const validateIsbn =
-    validateLength("ISBN", inputIsbn, 13, 20, true) && validateCodeIsbn(inputIsbn);
+    validateLength("ISBN", inputIsbn, 13, 20, true) &&
+    validateCodeIsbn(inputIsbn);
+  const validateStock =
+    validateLength("stock", inputStock, 1, 6, true) &&
+    validateNumber(inputStock);
+  const validatePags =
+    validateLength("total de paginas", inputPags, 1, 6, true) &&
+    validateNumber(inputPags);
+  const validatePrice =
+    validateLength("precio", inputPrice, 1, 7, true) &&
+    validateNumber(inputPrice);
+  const validateDimensions =
+    validateLength("formato de dimension", inputDimensions, 6, 15, true) &&
+    validateFormDimensions(inputDimensions);
   const validateTitle = validateLength("titulo", inputTitle, 2, 30, true);
   const validateAuthor = validateLength("autor", inputAuthor, 2, 30, true);
 
-  if (validateFromBookUrl && validateTitle && validateAuthor && validateIsbn) {
+  if (
+    validateFromBookUrl &&
+    validateTitle &&
+    validateAuthor &&
+    validateIsbn &&
+    validatePags &&
+    validatePrice &&
+    validateStock &&
+    validateDimensions
+  ) {
     return true;
   }
 };
