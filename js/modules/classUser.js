4f50902ac7a94214704180fb2df386388f0cb434
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-08-21 12:24:08 -0300

    add: creamos la clase constructora User(userName, userPassword, isAdmin)

---
+++ js/modules/classUser.js
@@ -0,0 +1,44 @@
+"use strict";
+
+class User {
+  #userName;
+  #userPassword;
+  #isAdmin;
+
+  constructor(userName, userPassword, isAdmin = false) {
+    this.#userName = userName;
+    this.#userPassword = userPassword;
+    this.#isAdmin = isAdmin;
+  }
+
+  get userName() {
+    return this.#userName;
+  }
+  get userPassword() {
+    return this.#userPassword;
+  }
+  get isAdmin() {
+    return this.#isAdmin;
+  }
+
+  set userName(newUserName) {
+    this.#userName = newUserName;
+  }
+  set userPassword(newPassword) {
+    this.#userPassword = newPassword;
+  }
+  set isAdmin(newValue) {
+    this.#isAdmin = newValue;
+  }
+
+  toJSON() {
+    return {
+      userName: this.userName,
+      userPassword: this.userPassword,
+      isAdmin: this.isAdmin,
+    };
+  }
+}
+
+const inviteUser = new User("invite", "invitePassword");
+const adminUser = new User("admin", "adminPassword", true);
