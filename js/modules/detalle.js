ef319802a57e1095ea4d59668970c3b8c452b73c
Augusto Brito <britoaugusto1@gmail.com>
2024-08-24 14:00:05 -0300

    feat: se creo una funcion para buscar el libro en el localstorage segun su id

--- js/modules/detalle.js
+++ js/modules/detalle.js
@@ -1,7 +1,11 @@
-//  import Book from "./modules/classBook.js";
-//  import { books } from "./main.js";
-import { showModalLogin } from "./modalLogin";
-// import { setInputsValue } from "./modules/validationAdmin.js";
+ import Book from "./classBook.js";
+ import { books } from "../main.js";
+
 
 const paramId = new URLSearchParams(window.location.search).get("id");
 console.log(paramId)
+
+ const libroBuscado = books.find(
+   (book) => book.id === paramId
+ );
+ console.log(libroBuscado)
