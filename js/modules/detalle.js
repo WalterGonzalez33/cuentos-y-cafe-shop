f2f79223e53671e8d68c4f72d0b3d4aedeee4338
Augusto Brito <britoaugusto1@gmail.com>
2024-08-24 13:02:24 -0300

    feat: se añadio un nuevo nav a la pagina detalle y se agrego el margin correspondiente al main

---
+++ js/modules/detalle.js
@@ -0,0 +1,7 @@
+//  import Book from "./modules/classBook.js";
+//  import { books } from "./main.js";
+import { showModalLogin } from "./modalLogin";
+// import { setInputsValue } from "./modules/validationAdmin.js";
+
+const paramId = new URLSearchParams(window.location.search).get("id");
+console.log(paramId)
