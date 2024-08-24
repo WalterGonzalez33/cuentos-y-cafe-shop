9fd809c73b410358499922974b701745dfc0c960
Augusto Brito <britoaugusto1@gmail.com>
2024-08-24 15:17:09 -0300

    se crearon las constantes para obtener mediante clases cada elemento del DOM a modificar

--- js/modules/detalle.js
+++ js/modules/detalle.js
@@ -3,9 +3,29 @@
 
 
 const paramId = new URLSearchParams(window.location.search).get("id");
-console.log(paramId)
-
  const libroBuscado = books.find(
    (book) => book.id === paramId
  );
- console.log(libroBuscado)
+
+ if (libroBuscado) {
+   const imgProducto = document.querySelector(
+     ".main-detalle .row.container-fluid img"
+   );
+   const titulo = document.querySelector(".main-detalle .col-lg-4 h2");
+   const categoria = document.querySelector(".main-detalle .col-lg-4 p.fs-5");
+   const autor = document.querySelector(".main-detalle .col-lg-4 p.fs-3");
+   const precio = document.querySelector(
+     ".main-detalle .col-lg-4 p.fw-bold.fs-4"
+   );
+   const sinopsis = document.querySelector(
+     ".main-detalle .col-12.col-md-6.col-lg-4.mt-lg-0 .fw-semibold.mt-4"
+   );
+   const fichaTecnica = document.querySelectorAll(
+     ".fondo-detalle .table tbody tr"
+   );
+
+
+   
+ } else {
+   console.error("Libro no encontrado");
+ }
